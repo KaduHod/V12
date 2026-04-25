0cbb126e3efcdb5767c1f26b5e96c561dfef4786
kaduhod <carlosjr.ribas@gmail.com.br>
2026-04-24 22:22:06 -0300

    Adicionado input para marcar valor de excluir e corrigir erro que não possibilita v12 de exluir dados

--- public/scripts/form.js
+++ public/scripts/form.js
@@ -19,10 +19,21 @@ function adicionar_item() {
         ITENS_CONTAINER().insertAdjacentHTML('afterbegin', novo_html);
         const novo_item = ITENS_CONTAINER().querySelector(`div[data-id="${novo_id}"]`);
         novo_item.querySelector('input[type="hidden"][name="id[]"]').value = novo_id;
+        novo_item.querySelector('input[data-remove-item]').addEventListener('change', gerencia_exluir);
         htmx.process(novo_item);
     }
+}
+/**
+ * Callback para evento de alteração em um checkbox.
+ * Atualiza o valor de um input hidden 'excluir[]' conforme o estado do checkbox.
+ * @param {Event} event - O objeto de evento do DOM.
+ */
+function gerencia_exluir(event) {
+    event.target.parentNode.querySelector("input[type='hidden'][name='excluir[]']").value = event.target.checked;
 }
 export default function loadEvents() {
     const add_item = document.querySelectorAll("button[data-id='adicionar-item'");
     add_item.forEach(e => e.addEventListener('click', adicionar_item));
+    const data_item = document.querySelectorAll('div[data-item="true"]');
+    data_item.forEach(e => e.querySelector('input[data-remove-item]').addEventListener('change', gerencia_exluir));
 }
